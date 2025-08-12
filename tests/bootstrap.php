5ab6dea42881dad1f5dd240f78c93f136bda0a0d
Kyrylo Dorozhynskyi <kyrylo.dorozhynskyi@justdev.org>
2025-08-12 13:48:19 +0200

    [HOTFIX] Fix plugin

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -23,7 +23,7 @@ require_once $_tests_dir . '/includes/functions.php';
  */
 function _manually_load_plugin()
 {
-	require dirname(dirname(__FILE__)) . '/jd-support-new.php';
+	require dirname(dirname(__FILE__)) . '/jd-support.php';
 }
 tests_add_filter('muplugins_loaded', '_manually_load_plugin');
 
